048c4386af69061725d709cc24de53074df29888
Chris Davitt <ChrisDavitt3@gmail.com>
2022-02-10 00:06:10 -0500

    decimal not working

--- drone_inventory/models.py
+++ drone_inventory/models.py
@@ -1,3 +1,4 @@
+from dataclasses import fields
 from email.policy import default
 from enum import unique
 from lib2to3.pgen2 import token
@@ -7,13 +8,23 @@ import secrets
 
 #3rd party imports
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin
+from flask_marshmallow import Marshmallow
+from sqlalchemy import ForeignKey
+
 
 # Adding flask security for passwords
 from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
+login_manager = LoginManager()
+ma = Marshmallow()
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+class User(db.Model, UserMixin):
     id = db.Column(db.String, primary_key = True)
     first_name = db.Column(db.String(50), nullable = True, default = '')
     last_name = db.Column(db.String(50), nullable = True, default = '')
@@ -22,6 +33,8 @@ class User(db.Model):
     g_auth_verify = db.Column(db.Boolean, default = False)
     token = db.Column(db.String, default='', unique = True)
     data_created = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+
+    car = db.relationship('Car', backref="owner", lazy=True)
 
     def __init__(self, email, first_name='', last_name='', id='', password='', token='', g_auth_verify=False):
         self.id = self.set_id()
@@ -45,3 +58,44 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User {self.email} has been added to the database.'
+
+class Car(db.Model):
+    id = db.Column(db.String, primary_key = True)
+    make = db.Column(db.String(50))
+    model = db.Column(db.String(200))
+    series = db.Column(db.String(100))
+    year = db.Column(db.String(5))
+    engine = db.Column(db.String(100))
+    msrp = db.Column(db.Numeric(precision=10, scale=2), nullable=True)
+    description = db.Column(db.String(150), nullable=True)
+    seats = db.Column(db.String(20), nullable=True)
+    weight = db.Column(db.String(50), nullable=True)
+    cost_of_production = db.Column(db.Numeric(precision=10, scale=2), nullable=True)
+    user_token = db.Column(db.String(200), db.ForeignKey('user.token'), nullable = False)
+
+    def __init__(self, make, model,series,year,engine,msrp, description, seats, weight, cost_of_production,user_token, id=''):
+        self.id = self.set_id()
+        self.make = make
+        self.model = model
+        self.series = series
+        self.year = year
+        self.engine = engine
+        self.msrp = msrp
+        self.description = description
+        self.weight = weight
+        self.cost_of_production = cost_of_production
+        self.seats = seats
+        self.user_token = user_token
+
+    def set_id(self):
+        return secrets.token_urlsafe()
+
+    def __repr__(self):
+        return f"The following Car has been created: {self.year} {self.make} {self.model} {self.series}."
+
+class CarSchema(ma.Schema):
+    class Meta:
+        fields = ['id', 'make', 'model','series','year','engine',  'msrp', 'description','seats','weight', 'cost_of_production']
+
+car_schema = CarSchema()
+cars_schema = CarSchema(many=True)
--- drone_inventory/site/routes.py
+++ drone_inventory/site/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-
+from flask_login import login_required
 
 site = Blueprint('site', __name__, template_folder='site_templates')
 """
@@ -14,3 +14,8 @@ which Flask uses to locate the Blueprints resource
 @site.route('/')
 def home():
     return render_template('index.html')
+
+@site.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html')
